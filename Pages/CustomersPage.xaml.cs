c3e92cafc53e8932ebd1e0178aad76b701a32d9f
Matthew MacKay <W0437676@campus.nscc.ca>
2024-03-03 12:37:18 -0400

    Add project files.

---
+++ Pages/CustomersPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EX3MM.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EX3MM.Pages
+{
+    /// <summary>
+    /// Interaction logic for CustomersPage.xaml
+    /// </summary>
+    public partial class CustomersPage : Page
+    {
+        NorthwindContext context = new NorthwindContext();
+        CollectionViewSource customerViewSource = new CollectionViewSource();
+
+        public CustomersPage()
+        {
+            InitializeComponent();
+
+            //Tie the markup viewsource object to the code viewsource object
+            customerViewSource = (CollectionViewSource)FindResource(nameof(customerViewSource));
+
+            //Use the dbcontext to tell EF to load data from the db
+            context.Customers.Load();
+
+            //Set the viewsource data source to use the customer data collection
+            customerViewSource.Source = context.Customers.Local.ToObservableCollection();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Pages/ProductsPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EX3MM.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EX3MM.Pages
+{
+    /// <summary>
+    /// Interaction logic for ProductsPage.xaml
+    /// </summary>
+    public partial class ProductsPage : Page
+    {
+        NorthwindContext context = new NorthwindContext();
+        CollectionViewSource productViewSource = new CollectionViewSource();
+
+        public ProductsPage()
+        {
+            InitializeComponent();
+            //Tie the markup viewsource object to the code viewsource object
+            productViewSource = (CollectionViewSource)FindResource(nameof(productViewSource));
+
+            //Use the dbcontext to tell EF to load data from the db
+            context.Products.Load();
+
+            //Set the viewsource data source to use the customer data collection
+            productViewSource.Source = context.Products.Local.ToObservableCollection();
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+    }
+}
